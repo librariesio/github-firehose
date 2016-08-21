64b5bd1c0807ca4a29df2fd2932dea0fa5d06299
Andrew Nesbitt <andrewnez@gmail.com>
2016-08-21 08:34:33 +0100

    Removed a whole bunch of dependencies

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 /*jshint strict:true, trailing:false, unused:true, node:true */
 'use strict';
 
-require("babel/register");
-
 var express      = require('express');
-var bodyParser   = require('body-parser');
-var serve_static = require('serve-static');
-var debug        = require('debug')('firehose');
 var router       = require('./lib/router');
 var cors         = require('cors')
 
 var app = express();
-app.use(bodyParser.json());
-app.use(serve_static('static'));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 var Firehose = require('./lib/github-firehose');
 
@@ -32,5 +22,5 @@ app.get('/events', cors(), router.firehose.tx);
 
 var port = process.env.PORT || 5001;
 app.listen(port, function() {
-  debug('Listening on', port);
+  console.log('Listening on', port);
 });
